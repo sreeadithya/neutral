8c0a5515f3fcc032278d06d9b4e5f5076256dafb
sreeadithya <79783628+sreeadithya@users.noreply.github.com>
2023-05-28 03:33:29 +0530

    good stuf (now line height of todo stored)
    when text overflows to next line it keeps flickering tho

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -3,12 +3,11 @@
 	import {
 		getAuth,
 		signInWithPopup,
-		GoogleAuthProvider } from "firebase/auth";
-		
-		import { getPerformance } from "firebase/performance";
+		GoogleAuthProvider } from "firebase/auth";		
 		
 		import { shortcut } from "./misc.js"
-		import { onMount } from 'svelte';
+		import { onMount, afterUpdate } from 'svelte';
+		let element
 		
 		import { getDatabase, ref, set, get, child } from "firebase/database";
 		
@@ -29,12 +28,41 @@
 		const auth = getAuth(app)
 		const provider = new GoogleAuthProvider(app);
 		const user = auth.currentUser;
-		const perf = getPerformance(app);
 		
 		const db = getDatabase();
 		
+		let displayWebElements = 'none'
+		let displayWebElementsText = 'none'
+		let displayMobileElements = 'none'
 		
 		
+		function detectMob() {
+			const toMatch = [
+			/Android/i,
+			/webOS/i,
+			/iPhone/i,
+			/iPad/i,
+			/iPod/i,
+			/BlackBerry/i,
+			/Windows Phone/i
+			];
+			
+			return toMatch.some((toMatchItem) => {
+				return navigator.userAgent.match(toMatchItem);
+			});
+		}
+		console.log(detectMob())
+		
+		if (detectMob()) {
+			displayWebElements = 'none'
+			displayWebElementsText = 'none'
+			displayMobileElements = 'flex'
+		}
+		else {
+			displayWebElements = 'flex'
+			displayWebElementsText = 'block'
+			displayMobileElements = 'none'
+		}
 		
 		let userId = null;
 		let displayName
@@ -46,6 +74,14 @@
 		let showMenuItems = 'none'
 		let showShortcuts = 'none'
 		
+		let userInput = ''
+		let showInputField = 'none';
+		let focusInputField = false
+		let inputRef;		
+		let moreInfoDisplay = 'none'
+		
+		let todoHeight = '23px'
+		let subTextHeight = 0
 		
 		function getWelcomeText() {
 			const now = new Date();
@@ -62,9 +98,34 @@
 			}
 		}
 		
+		let todoHeightUpdated
 		
-		let showUserName = 'block'
-		showUserName = JSON.parse(localStorage.getItem('showUserName'))
+		
+		function updateTodoHeight(event, index) {
+			
+			let el = event.target
+
+			todoHeightUpdated = (el.scrollHeight > el.clientHeight) ? (el.scrollHeight) + "px" : '23px';
+			
+			
+			console.log(todoHeightUpdated)
+			
+			
+			todos[index].todoHeight = todoHeightUpdated
+
+			writeUserData()
+			
+		}
+		
+		
+		var showUserNameStorage = JSON.parse(localStorage.getItem('showUserName'))
+		let showUserName
+		if (showUserNameStorage == 'none') {
+			showUserName = 'none'
+		}
+		else {
+			showUserName = 'block'
+		}
 		
 		var isDarkModeStorage = JSON.parse(localStorage.getItem('isDarkModeStorage'))
 		let isDarkMode
@@ -166,13 +227,9 @@
 		
 		
 		
-		
-		
-		
-		
 		function addTodo() {
-			todos = [{todo:'', completed: false, subtext: '', showSubText: 'none'}, ...todos]
 			writeUserData()
+			
 		}
 		
 		
@@ -187,30 +244,21 @@
 		
 		
 		
-		function autoGrow(event) {
+		
+		function autoGrowSubText(event) {
 			const element = event.target;
 			element.style.height = '5px';
 			element.style.height = element.scrollHeight + 'px';
+			console.log(element.style.height)
 		}
-		
-		
-		
-		onMount(() => {
-			
-			
-			document.addEventListener('keydown', handleKeyDown);
-			return () => {
-				document.removeEventListener('keydown', handleKeyDown);
-			};
-			
-		});
 		
 		function handleKeyDown(event) {
 			if (event.ctrlKey && event.code === 'Space') {
-				addTodo();
+				focusInputField = true
+				showInputField = 'block'
 			}
 			
-			if (event.ctrlKey && event.code === 'KeyO') {
+			if (event.ctrlKey && event.code === 'KeyU') {
 				event.preventDefault()
 				if (showUserName == 'block') {
 					showUserName = 'none'	
@@ -249,18 +297,28 @@
 				event.preventDefault()
 				console.log(todos)
 				
-				
 			}
 			
 			
 		}
 		
+		onMount(() => {
+			
+			
+			document.addEventListener('keydown', handleKeyDown);
+			return () => {
+				document.removeEventListener('keydown', handleKeyDown);
+			};
+			
+		});
 		
-		
-		
-		
-		
-		
+		afterUpdate(() => {
+			if (focusInputField) {
+				inputRef.focus();
+				focusInputField = false;
+			}
+			
+		});
 		
 		
 	</script>
@@ -278,163 +336,221 @@
 			</p>
 			
 			
-			<button id="logInButton" on:click|preventDefault={logInButton}>log in</button>
+			<button class="defaultButton" on:click|preventDefault={logInButton} style="display: {displayWebElements};">log in</button>
 			
 			
 			
-			<div id="bottomElements">
-				<p class="homePageText" >use in your browser by logging in here or download the android or windows app.</p>
-				<p id="bottomElementsCredit" class="homePageText">made by translate</p>
+			<div class="bottomElements" style="padding-top: 13vh;">
+				
+				<div style="display: flex; flex-direction: column">
+					<p class="homePageText" style="display: {displayWebElementsText}"> use in your browser by logging in or <u>download for windows</u> </p>
+					<p class="homePageText" style="display: {displayMobileElements};" >the app is currently only available for using on a web browser on a pc or as a windows native app. <br> an android version is in the works</p>
+					
+					<p on:click={ () => {
+						whenSignedOutDisplay = 'none'
+						moreInfoDisplay = 'flex'
+					} } on:keydown style='cursor: pointer;'> <u>more info</u>  </p>
+				</div>
+				
+				
+				<p class="bottomElementsCredit" >  made by <u> <a href="https://github.com/sreeadithya" target="_blank">translate</a></u> </p>
 			</div>
 			
 		</section>
 		
+		<section id="moreInfo" style="display: {moreInfoDisplay}; flex-direction: column;">
+			
+			<span class="homePageText">hi</span>
+			<p class="homePageText" id="moreInfoText">
+				neutral is a hobby project that i’ve created out of curiosity and boredom. i hope you enjoy the experience i have created. <br><br><br>
+				the entire project is open source here <br> <br><br>
+				web version built on svelte and firebase (auth and realtime db), hosted on netlify <br><br><br>
+				desktop version made using tauri
+			</p>
+			<br>
+			
+			
+			
+			<div class="bottomElements" style="padding-top: 13vh;">
+				<button on:click={() => {
+					moreInfoDisplay = 'none'
+					whenSignedOutDisplay = 'flex'
+				}} class='defaultButton' id="backButton"> back </button>
+				
+				<p class="bottomElementsCredit" style="padding-bottom: 0px; margin-bottom: 0px;">
+					made by <u> <a href="https://github.com/sreeadithya" target="_blank">translate</a></u> <br>
+					buy me a coffee
+				</p>
+				
+			</div>
+			
+			
+			
+		</section>
 		
 		<section id="whenSignedIn" style="display: {whenSignedInDisplay};">
 			
 			<div id="leftSide">
-				{#each todos as {todo, completed, subtext, showSubText}, index}
-				
-				<div class="todoAndCheckBox">
-					<label class="checkBoxContainer">
-						<input class="checkBox" type="checkbox" bind:checked={completed} on:input={writeUserData}
+				<form on:submit|preventDefault={ () => { 
+					if(userInput.trim().length) todos = [{todo:userInput, completed: false, subtext: '', showSubText: 'none'}, ...todos];
+					userInput = '';  showInputField = 'none'; document.getElementById('inputArea').style.display='block'; addTodo();
+				} }
+				id="inputArea" style="display: {showInputField};" autocomplete="off" spellcheck="false" >
+				<input type="text" bind:value={userInput} id="todoInputArea" bind:this={inputRef}>
+			</form>
 			
-						
-						
-						value=true>
-						<span class="checkBoxIndicator"></span>
-					</label>					
-					<textarea class="todos" on:input={writeUserData} type="text" bind:value={todo} class:completedTodo={completed} use:shortcut={{
-						
-						control: true,
-						code: 'KeyD',
-						callback: () => removeTodo(index),
-						
-						code2: 'Enter',
-						callback2: () => {
-							showSubText = 'block'
-							writeUserData()
-						},
-						
-						code4: 'Backspace',
-						callback4: () => {
-							if (todo === '') {
-								removeTodo(index)
-							}
-						},
-						shift: true,
-						callback5: () => {
-							todo = todo + '\n'
-							writeUserData
-						},
-
-						code6: 'KeyC',
-							callback6: () => {
-								console.log('lmao')
-								completed=!completed
-								
-								writeUserData()
-							},
-							
-						
-						todo: todo
-						
-					}} on:input={autoGrow}></textarea>
-				</div>
-				
-				
-				
-				<textarea
-				on:input={autoGrow}
-				class="subText"
-				class:completedSubText={completed}
+			{#each todos as {todo, completed, subtext, showSubText, todoHeight, subTextHeight}, index}
+			
+			<div class="todoAndCheckBox">
+				<label class="checkBoxContainer">
+					<input class="checkBox" type="checkbox" bind:checked={completed} on:input={writeUserData}
+					
+					
+					spellcheck="false" 
+					value=true>
+					<span class="checkBoxIndicator"></span>
+				</label>					
+				<textarea   class="todos"
+				bind:value={todo}
+				on:input={ event => {
+					updateTodoHeight(event, index)
+					
+					
+				} }
 				type="text"
-				bind:value={subtext}
-				placeholder="_"
-				style="display: {showSubText}"
-				on:input={writeUserData}
-				use:shortcut={{
-					code3: 'Backspace',
-					callback3: () => {
-						if (subtext === '') {
-							showSubText = 'none';
+				class:completedTodo={completed}
+				style="height: {todoHeight}"
+				spellcheck="false" use:shortcut={{
+					
+					control: true,
+					code: 'KeyD',
+					callback: () => removeTodo(index),
+					
+					code2: 'Enter',
+					callback2: () => {
+						showSubText = 'block'
+						writeUserData()
+					},
+					
+					code4: 'Backspace',
+					callback4: () => {
+						if (todo === '') {
+							removeTodo(index)
+							
 						}
 					},
-					subtext: subtext,
-				}}></textarea>
-				
-				
-				
-				<br>
-				
-				
-				
-				
-				{/each}
-			</div>
-			
-			
-			<div id="rightSide" style=" display: {showUserName} "   >
-				
-				<a href="/" on:click|preventDefault={ () => {
-					if (showMenuItems == 'none') {
-						showMenuItems = 'flex'
-					}
-					else if (showMenuItems == 'flex') {
-						showMenuItems = 'none'
-					}
 					
-				} }>
-				<p class="homePageText" id="displayName" style="color: #B0B0B0; text-align: right"> {displayName} </p>
-			</a>
-			
-			
-			
-			<div id="menuItems" style="display: {showMenuItems};">
-				
-				<a href="/" on:click|preventDefault={ () => {
-					if (showShortcuts == 'none') {
-						showShortcuts = 'flex'
-					}
-					else if (showShortcuts == 'flex') {
-						showShortcuts = 'none'
-					}
-				}}>
-				<p class="homePageText" > how to use </p>
-			</a>
-			
-			<div id="shortcutsMenu" style="display: {showShortcuts};">
-				<p style="line-height: 1.7em">
-					<i> get started: </i>  <br>
-					ctrl + space to create new task <br>
-					shift + enter to create new line <br>
-					press enter on task to add sub text <br>
-					hold ctrl to drag block <br> <br>
-					<i> remember these:</i>  <br>
-					ctrl + c to check selected <br>
-					ctrl + d to delete selected <br>
-					ctrl + o to hide sidebar (imp)<br>
-					ctrl + b to blur screen <br>
-					ctrl + k for dark mode <br>  <br>
-
-					<i> made by <a href="https://github.com/sreeadithya" target="_blank"> <u>translate</u>  </a> </i>  <br>
-					<i> <a href="https://ko-fi.com/translate" target="_blank"> <u>buy me a coffee</u> </a></i> 
-				</p>
+						shift: true,
+							callback5: () => {
+								todo = todo + '\n'
+							},
+					code6: 'KeyC',
+					callback6: () => {
+						console.log('lmao')
+						completed=!completed
+						
+						writeUserData()
+					},
+					
+					
+					todo: todo
+					
+				}}></textarea>
 			</div>
 			
-			<a href="/" on:click={signOutButton}>
-				<p class="homePageText" > sign out </p>
-			</a>
+			
+			
+			<textarea
+			spellcheck="false" 
+			
+			class="subText"
+			class:completedSubText={completed}
+			type="text"
+			bind:value={subtext}
+			placeholder=""
+			style="display: {showSubText}"
+			on:input={writeUserData}
+			use:shortcut={{
+				code3: 'Backspace',
+				callback3: () => {
+					if (subtext === '') {
+						showSubText = 'none';
+						writeUserData()
+					}
+				},
+				subtext: subtext,
+			}}></textarea>
+			
+			
+			
+			<br>
 			
 			
 			
 			
-			
-			
+			{/each}
 		</div>
 		
 		
+		<div id="rightSide" style=" display: {showUserName}; "   >
+			
+			<a href="/" on:click|preventDefault={ () => {
+				if (showMenuItems == 'none') {
+					showMenuItems = 'flex'
+				}
+				else if (showMenuItems == 'flex') {
+					showMenuItems = 'none'
+				}
+				
+			} }>
+			<p class="homePageText" id="displayName" style="color: #B0B0B0; text-align: right"> {displayName} </p>
+		</a>
+		
+		
+		
+		<div id="menuItems" style="display: {showMenuItems};">
+			
+			<a href="/" on:click|preventDefault={ () => {
+				if (showShortcuts == 'none') {
+					showShortcuts = 'flex'
+				}
+				else if (showShortcuts == 'flex') {
+					showShortcuts = 'none'
+				}
+			}}>
+			<p class="homePageText" > how to use </p>
+		</a>
+		
+		<div id="shortcutsMenu" style="display: {showShortcuts};">
+			<p style="line-height: 1.7em">
+				<i> get started: </i>  <br>
+				ctrl + space to create new task <br>
+				hit enter on task to add sub text <br>
+				<i> remember these:</i>  <br>
+				ctrl + c to check selected <br>
+				ctrl + d to delete <br>
+				ctrl + u to hide username<br>
+				ctrl + b to blur screen <br>
+				ctrl + k for dark mode <br>  <br>
+				
+				<i> made by <a href="https://github.com/sreeadithya" target="_blank"> <u>translate</u>  </a> </i>  <br>
+				<i> <a href="https://ko-fi.com/translate" target="_blank"> <u>buy me a coffee</u> </a></i> 
+			</p>
+		</div>
+		
+		<a href="/" on:click={signOutButton}>
+			<p class="homePageText" > sign out </p>
+		</a>
+		
+		
+		
+		
+		
+		
 	</div>
+	
+	
+</div>
 </section>
 </main>
 
@@ -451,8 +567,9 @@
 		font-size: 18px;
 		font-family: Helvetica, Arial, sans-serif;
 		margin: 5%;
+		margin-top: 7%;
 	}
-
+	
 	
 	:global(body) {
 		background-color: #FFFFFF;
@@ -464,9 +581,13 @@
 		color: #FFFFFF;
 	}
 	
-	:global(body.darkMode) #logInButton {
+	:global(body.darkMode) .defaultButton {
 		background-color: #121212;
 		color: #FFFFFF;
+	}
+	
+	:global(body.darkMode) #rightSide {
+		background-color: #121212;
 	}
 	
 	:global(body.darkMode) #shortcutsMenu {
@@ -485,42 +606,42 @@
 		accent-color: #FFFFFF;
 		
 	}
-
+	
 	:global(body.darkMode) .completedTodo {
 		margin: 0px;
 		padding-top: 0px;
 		padding-bottom: 2px;
 		color: #FFFFFF80;
 	}
-
+	
 	:global(body.darkMode) .completedSubText {
 		padding-bottom: 0px;
 		margin: 0px;
 		margin-left: 50px;
 		color: #B0B0B0;
 	}
-
-
-
+	
+	
+	
 	:global(body.darkMode) .checkBoxContainer input:checked ~ .checkBoxIndicator {
 		color: #FFFFFF;
 		
 		
 	}
-		.completedTodo {
+	.completedTodo {
 		margin: 0px;
 		padding-top: 0px;
 		padding-bottom: 2px;
 		color: #B0B0B0;
 	}
-
+	
 	.completedSubText {
 		padding-bottom: 0px;
 		margin: 0px;
 		margin-left: 50px;
 		color: #B0B0B0;
 	}
-
+	
 	
 	
 	
@@ -571,9 +692,10 @@
 	
 	textarea {
 		color: #000000;
+		overflow-y: hidden;
 	}
 	
-	#logInButton {
+	.defaultButton {
 		
 		outline: none;
 		border-color: #B0B0B0;
@@ -585,6 +707,7 @@
 		background-color: #FFFFFF;
 		margin-top: 10vh;
 		color: #000000;
+		padding-left: 0.9em;
 		
 		
 	}
@@ -615,13 +738,16 @@
 		
 		padding: 0px;
 		margin: 0px;
-
+		position: absolute;
+		right: 0;
+		z-index: 10;
+		background-color: #FFFFFF;
 		
 	}
-
+	
 	#rightSide:hover {
-	  display: block;
-
+		display: block;
+		
 	}
 	
 	
@@ -635,6 +761,7 @@
 		align-items: flex-start;
 		margin: 0px;
 		padding: 0px;
+		
 	}
 	
 	
@@ -644,6 +771,12 @@
 		padding-top: 10vh;
 		width: 70vw;
 	}
+	
+	#moreInfoText {
+		padding-top: 10vh;
+		width: 70vw;
+	}
+	
 	
 	.todos {
 		margin: 0px;
@@ -657,27 +790,62 @@
 		padding-bottom: 2px;
 		color: #B0B0B0;
 	}
-
+	
 	.completedSubText {
 		padding-bottom: 0px;
 		margin: 0px;
 		margin-left: 50px;
 		color: #B0B0B0;
 	}
-
+	
 	.subText {
 		padding-bottom: 0px;
 		margin: 0px;
 		margin-left: 50px;
 	}
 	
-	#bottomElements {
-		padding-top: 25vh;
+	.bottomElements {
+		padding-top: 20vh;
+		padding-bottom: 10vh;
 		display: flex;
 		flex-flow: row wrap;
 		justify-content: space-between;
+		height: 100%;
+		bottom: 0;
+		
 	}
 	
+	
+	.bottomElementsCredit {
+		display: inline-block;
+		align-self: flex-end;
+	}
+	
+	
+	@media screen and (max-height: 790px) {
+		.bottomElements {
+			padding-top: 8vh;
+		}
+	}
+	
+	@media screen and (max-width: 790px) {
+		.bottomElements {
+			padding-top: 6vh;
+		}
+	}
+	
+	@media screen and (max-width: 790px) {
+		.defaultButton {
+			margin-top: 6vh;
+		}
+	}
+	
+	#backButton {
+		padding-left: 15px;
+		padding-right: 15px;
+		display: inline-block;
+		align-self: flex-end;
+	}
 	
 	#displayName {
 		padding: 0px;
@@ -730,5 +898,7 @@
 		display: flex;
 		flex-direction: column;
 	}
+	
+	
 	
 </style>
